637a30f7b48045aed4a38c3e421fcd1dc7c94ce4
Leo Cheung <lappangcheung@gmail.com>
2023-11-12 16:13:54 -0500

    Setting up the base projects

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), eslintPlugin()],
+  server: {
+    port: 1337
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    },
+    extensions: ['.vue', '.js'],
+  }
+})
